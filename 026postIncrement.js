2eb221af9ef388cb0440be55a866ff84df9c359c
Devesh <deveshachaudhari@gmail.com>
2023-07-26 13:07:52 +0530

    26

---
+++ 026postIncrement.js
@@ -0,0 +1,13 @@
+// Examine post increment operator
+let x = 5
+let y = x++
+console.log(x, y);
+
+let p = 5
+let q = p++ * 5
+console.log(p, q);
+
+let a = 5
+let b = a++
+let c = (b++ % a)
+console.log(a,b,c);
